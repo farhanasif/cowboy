9fedf7f017eeff8071fe1f392d92d1b70f305d53
Farhan Asif <f.asif2010@gmail.com>
2023-06-21 20:05:20 +0600

    small tweak

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,17 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-        Hello world!
-      </h1>
+      <div className="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25">
+        
+        <div className="relative rounded-xl overflow-auto p-6">
+          <div className="text-xl sm:text-5xl font-extrabold leading-none tracking-tight text-center">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+              Hello world
+            </span>
+          </div>
+        </div>
+        <div class="absolute inset-0 pointer-events-none border border-black/5 rounded-xl dark:border-white/5"></div>
+      </div>
     </main>
   )
 }
